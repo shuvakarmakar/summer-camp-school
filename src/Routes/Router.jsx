a8660d0b478696815b6a0556e89e62c0b77bc139
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-06-07 17:12:10 +0600

    Error Page Done

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Error from "../Pages/Error/Error";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Home/Login/Login";
 import SignUp from "../Pages/Home/SignUp/SignUp";
@@ -23,4 +24,8 @@ export const router = createBrowserRouter([
         }
       ]
     },
+    {
+        path: '*',
+        element: <Error></Error>
+    }
   ]);
