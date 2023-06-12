8aa65c4ce6b4e97eab9fd32119cf869ffbc72709
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-06-12 10:00:48 +0600

    Some Animation Added in Slider

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -11,6 +11,7 @@ import img3 from '../../../assets/Banner/camp3.jpg';
 import img4 from '../../../assets/Banner/camp4.jpg';
 import img5 from '../../../assets/Banner/camp5.jpg';
 import img6 from '../../../assets/Banner/camp6.jpg';
+import { Flip, Roll, Rotate, Slide, Zoom } from 'react-awesome-reveal';
 
 const Banner = () => {
     return (
@@ -32,55 +33,67 @@ const Banner = () => {
                 <SwiperSlide className="relative">
                     <img src={img1} alt="Banner 1" className="w-full h-full object-cover" />
                     <div className="absolute inset-0 flex items-center justify-center bg-gradient-to-r from-gray-900 to-transparent">
-                        <div className="text-white text-center space-y-7">
-                            <h2 className="text-4xl md:text-6xl font-bold">Adventure Awaits</h2>
-                            <p>Experience the thrill of outdoor camping.</p>
-                        </div>
+                        <Slide direction='left'>
+                            <div className="text-white text-center space-y-7">
+                                <h2 className="text-4xl md:text-6xl font-bold">Adventure Awaits</h2>
+                                <p>Experience the thrill of outdoor camping.</p>
+                            </div>
+                        </Slide>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="relative">
                     <img src={img2} alt="Banner 2" className="w-full h-full object-cover" />
                     <div className="absolute inset-0 flex items-center justify-center bg-gradient-to-r from-gray-900 to-transparent">
-                        <div className="text-white text-center space-y-7">
-                            <h2 className="text-4xl md:text-6xl font-bold">Nature Exploration</h2>
-                            <p>Discover the wonders of the natural world.</p>
-                        </div>
+                        <Slide direction='left'>
+                            <div className="text-white text-center space-y-7">
+                                <h2 className="text-4xl md:text-6xl font-bold">Nature Exploration</h2>
+                                <p>Discover the wonders of the natural world.</p>
+                            </div>
+                        </Slide>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="relative">
                     <img src={img3} alt="Banner 3" className="w-full h-full object-cover" />
                     <div className="absolute inset-0 flex items-center justify-center bg-gradient-to-r from-gray-900 to-transparent">
-                        <div className="text-white text-center space-y-7">
-                            <h2 className="text-4xl md:text-6xl font-bold">Outdoor Activities</h2>
-                            <p>Engage in exciting adventures and outdoor sports.</p>
-                        </div>
+                        <Flip direction='left'>
+                            <div className="text-white text-center space-y-7">
+                                <h2 className="text-4xl md:text-6xl font-bold">Outdoor Activities</h2>
+                                <p>Engage in exciting adventures and outdoor sports.</p>
+                            </div>
+                        </Flip>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="relative">
                     <img src={img4} alt="Banner 4" className="w-full h-full object-cover" />
                     <div className="absolute inset-0 flex items-center justify-center bg-gradient-to-r from-gray-900 to-transparent">
-                        <div className="text-white text-center space-y-7">
-                            <h2 className="text-4xl md:text-6xl font-bold">Unwind in Nature</h2>
-                            <p>Escape the busy city life and relax in serene surroundings.</p>
-                        </div>
+                        <Roll direction='left' triggerOnce>
+                            <div className="text-white text-center space-y-7">
+                                <h2 className="text-4xl md:text-6xl font-bold">Unwind in Nature</h2>
+                                <p>Escape the busy city life and relax in serene surroundings.</p>
+                            </div>
+                        </Roll>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="relative">
                     <img src={img5} alt="Banner 5" className="w-full h-full object-cover" />
                     <div className="absolute inset-0 flex items-center justify-center bg-gradient-to-r from-gray-900 to-transparent">
-                        <div className="text-white text-center space-y-7">
-                            <h2 className="text-4xl md:text-6xl font-bold">Wildlife Encounters</h2>
-                            <p>Observe fascinating wildlife up close in their natural habitats.</p>
-                        </div>
+                        <Zoom direction='left'>
+                            <div className="text-white text-center space-y-7">
+                                <h2 className="text-4xl md:text-6xl font-bold">Wildlife Encounters</h2>
+                                <p>Observe fascinating wildlife up close in their natural habitats.</p>
+                            </div>
+                        </Zoom>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="relative">
                     <img src={img6} alt="Banner 6" className="w-full h-full object-cover" />
                     <div className="absolute inset-0 flex items-center justify-center bg-gradient-to-r from-gray-900 to-transparent">
-                        <div className="text-white text-center space-y-7">
-                            <h2 className="text-4xl md:text-6xl font-bold">Wilderness Adventures</h2>
-                            <p>Embark on thrilling journeys in untamed landscapes.</p>
-                        </div>
+                        <Rotate direction='left'>
+                            <div className="text-white text-center space-y-7">
+                                <h2 className="text-4xl md:text-6xl font-bold">Wilderness Adventures</h2>
+                                <p>Embark on thrilling journeys in untamed landscapes.</p>
+                            </div>
+                        </Rotate>
                     </div>
                 </SwiperSlide>
             </Swiper>
