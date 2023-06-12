4d12ceb208d119801dd0dac68c67cc2341c51bea
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-06-12 10:10:47 +0600

    Some Animation Added in Slider

--- src/Pages/Home/Banner/InstructorBanner.jsx
+++ src/Pages/Home/Banner/InstructorBanner.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Zoom } from "react-awesome-reveal";
 
 const InstructorBanner = () => {
     const [instructors, setInstructors] = useState([]);
@@ -13,22 +14,23 @@ const InstructorBanner = () => {
 
     return (
         <>
-            <div className="container mx-auto">
-                <h2 className="text-center text-4xl my-6 font-semibold">Popular Instructors</h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {instructors.map(instructor => (
-                        <div className="card w-96 bg-base-100 shadow-xl" key={instructor._id}>
-                            <figure className="px-10 pt-10">
-                                <img src={instructor.image} alt="Instructor" className="rounded-xl" />
-                            </figure>
-                            <div className="card-body items-center text-center">
-                                <h2 className="card-title">{instructor.name}</h2>
-                                <p>Email: {instructor.email}</p>
+            <Zoom>
+                <div className="container mx-auto">
+                    <h2 className="text-center text-4xl my-6 font-semibold">Popular Instructors</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {instructors.map(instructor => (
+                            <div className="card w-96 bg-base-100 shadow-xl" key={instructor._id}>
+                                <figure className="px-10 pt-10">
+                                    <img src={instructor.image} alt="Instructor" className="rounded-xl" />
+                                </figure>
+                                <div className="card-body items-center text-center">
+                                    <h2 className="card-title">{instructor.name}</h2>
+                                    <p>Email: {instructor.email}</p>
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
+                        ))}
+                    </div>
+                </div></Zoom>
         </>
     );
 };
--- src/Pages/Home/Banner/PopularCourses.jsx
+++ src/Pages/Home/Banner/PopularCourses.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Fade } from 'react-awesome-reveal';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const PopularCourses = () => {
@@ -19,23 +20,25 @@ const PopularCourses = () => {
     }, []);
 
     return (
-        <>
+        <div>
             <h2 className="text-center text-4xl my-6 font-semibold">Popular Courses</h2>
-            <div className='grid lg:grid-cols-3 gap-5'>
-                {popularClasses.map((classItem) => (
-                    <div key={classItem._id}>
-                        <div className="card w-96 h-96 glass lg:m-6">
-                            <figure><img src={classItem.image} alt="car!" /></figure>
-                            <div className="card-body">
-                                <h2 className="card-title">{classItem.className}</h2>
-                                <p>Instructor: {classItem.instructorName}</p>
-                                <p>Student Enrolled: {classItem.enrolled}</p>
+            <Fade direction='left'>
+                <div className='grid lg:grid-cols-3 gap-5'>
+                    {popularClasses.map((classItem) => (
+                        <div key={classItem._id}>
+                            <div className="card w-96 h-96 glass lg:m-6">
+                                <figure><img src={classItem.image} alt="car!" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{classItem.className}</h2>
+                                    <p>Instructor: {classItem.instructorName}</p>
+                                    <p>Student Enrolled: {classItem.enrolled}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-        </>
+                    ))}
+                </div>
+            </Fade>
+        </div>
     );
 };
 
