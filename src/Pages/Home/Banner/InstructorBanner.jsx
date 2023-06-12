d6f377dbbd0deb3ec1ff2d150f21f0c2743e2321
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-06-12 23:28:56 +0600

    Some Update of Popular Instructor

--- src/Pages/Home/Banner/InstructorBanner.jsx
+++ src/Pages/Home/Banner/InstructorBanner.jsx
@@ -12,13 +12,15 @@ const InstructorBanner = () => {
             });
     }, []);
 
+    const limitedInstructors = instructors.slice(0, 6);
+
     return (
         <>
             <Fade>
                 <div className="container mx-auto">
                     <h2 className="text-center text-4xl my-6 font-semibold">Popular Instructors</h2>
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {instructors.map(instructor => (
+                        {limitedInstructors.map((instructor) => (
                             <div className="card w-96 bg-base-100 shadow-xl" key={instructor._id}>
                                 <figure className="px-10 pt-10">
                                     <img src={instructor.image} alt="Instructor" className="rounded-xl" />
@@ -30,7 +32,8 @@ const InstructorBanner = () => {
                             </div>
                         ))}
                     </div>
-                </div></Fade>
+                </div>
+            </Fade>
         </>
     );
 };
